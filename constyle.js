e691f97de0fb73c225025988f515e92c843fa659
Dan Tao <daniel.tao@gmail.com>
2014-02-28 23:57:43 -0800

    added a bunch more colors + strikethrough

--- constyle.js
+++ constyle.js
@@ -2,30 +2,46 @@ var jsdom = require('jsdom').jsdom,
     nearestColor = require('nearest-color');
 
 var supportedColors = {
-  red: '#f00',
-  yellow: '#ff0',
+  white: '#fff',
+  grey: '#888',
+  black: '#000',
   blue: '#00f',
-  green: '#0f0'
+  cyan: '#0ff',
+  green: '#0f0',
+  magenta: '#f0f',
+  red: '#f00',
+  yellow: '#ff0'
 };
 
 var colorSequences = {
-  red: ['\x1B[31m', '\x1B[39m'],
-  yellow: ['\x1B[33m', '\x1B[39m'],
+  white: ['\x1B[37m', '\x1B[39m'],
+  grey: ['\x1B[90m', '\x1B[39m'],
+  black: ['\x1B[30m', '\x1B[39m'],
   blue: ['\x1B[34m', '\x1B[39m'],
-  green: ['\x1B[32m', '\x1B[39m']
+  cyan: ['\x1B[36m', '\x1B[39m'],
+  green: ['\x1B[32m', '\x1B[39m'],
+  magenta: ['\x1B[35m', '\x1B[39m'],
+  red: ['\x1B[31m', '\x1B[39m'],
+  yellow: ['\x1B[33m', '\x1B[39m']
 };
 
 var bgColorSequences = {
-  red: ['\x1B[41m', '\x1B[49m'],
-  yellow: ['\x1B[43m', '\x1B[49m'],
+  white: ['\x1B[47m', '\x1B[49m'],
+  grey: ['\x1B[49;5;8m', '\x1B[49m'],
+  black: ['\x1B[40m', '\x1B[49m'],
   blue: ['\x1B[44m', '\x1B[49m'],
-  green: ['\x1B[42m', '\x1B[49m']
+  cyan: ['\x1B[46m', '\x1B[49m'],
+  green: ['\x1B[42m', '\x1B[49m'],
+  magenta: ['\x1B[45m', '\x1B[49m'],
+  red: ['\x1B[41m', '\x1B[49m'],
+  yellow: ['\x1B[43m', '\x1B[49m']
 };
 
 var styleSequences = {
   bold: ['\x1B[1m',  '\x1B[22m'],
   italic: ['\x1B[3m',  '\x1B[23m'],
   underline: ['\x1B[4m', '\x1B[24m'],
+  strikethrough: ['\x1B[9m',  '\x1B[29m']
 };
 
 /**
@@ -36,7 +52,7 @@ var styleSequences = {
  * constyle('<span style="text-decoration: underline;">foo</span>');
  * // => '\x1B[4mfoo\x1B[24m'
  *
- * constyle('<html><style>.yellow { color: #ffe; }</style><span class="yellow">foo</span></html>');
+ * constyle('<html><style>.yellow { color: #ff4; }</style><span class="yellow">foo</span></html>');
  * // => '\x1B[33mfoo\x1B[39m'
  */
 function constyle(html) {
