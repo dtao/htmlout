a7a9d566829038be4a1eba904ec48249a4d7ae60
Dan Tao <daniel.tao@gmail.com>
2014-02-28 23:27:39 -0800

    added support for computed styles
    also: italic, bold, and underlined text

--- constyle.js
+++ constyle.js
@@ -8,44 +8,103 @@ var supportedColors = {
   green: '#0f0'
 };
 
-var escapeSequences = {
+var colorSequences = {
   red: ['\x1B[31m', '\x1B[39m'],
   yellow: ['\x1B[33m', '\x1B[39m'],
   blue: ['\x1B[34m', '\x1B[39m'],
   green: ['\x1B[32m', '\x1B[39m']
 };
 
+var bgColorSequences = {
+  red: ['\x1B[41m', '\x1B[49m'],
+  yellow: ['\x1B[43m', '\x1B[49m'],
+  blue: ['\x1B[44m', '\x1B[49m'],
+  green: ['\x1B[42m', '\x1B[49m']
+};
+
+var styleSequences = {
+  bold: ['\x1B[1m',  '\x1B[22m'],
+  italic: ['\x1B[3m',  '\x1B[23m'],
+  underline: ['\x1B[4m', '\x1B[24m'],
+};
+
 /**
  * @example
- * output('<span style="color: #f00;">Hello</span>, <span style="color: #0f0;">world</span>!');
+ * constyle('<span style="color: #f24;">Hello</span>, <span style="color: #4f4;">world</span>!');
  * // => '\x1B[31mHello\x1B[39m, \x1B[32mworld\x1B[39m!'
+ *
+ * constyle('<span style="text-decoration: underline;">foo</span>');
+ * // => '\x1B[4mfoo\x1B[24m'
+ *
+ * constyle('<html><style>.yellow { color: #ffe; }</style><span class="yellow">foo</span></html>');
+ * // => '\x1B[33mfoo\x1B[39m'
  */
-function output(html) {
-  var doc = jsdom(html);
+function constyle(html) {
+  var doc = jsdom(html),
+      win = doc.parentWindow;
 
-  var buffer = '';
+  var buffer = [];
   forEach(doc.childNodes, function(node) {
-    switch (node.nodeType) {
-      case 1: // element
-        buffer += applyStyle(node);
-        break;
-
-      case 3: // text node
-        buffer += node.textContent;
-        break;
-    }
+    output(node, win, buffer);
   });
 
-  return buffer;
+  return buffer.join('');
 }
 
-function applyStyle(node) {
+function output(node, win, buffer) {
+  if (node.nodeName === 'STYLE' || node.nodeName === 'SCRIPT') {
+    return;
+  }
+
+  if (hasChildren(node)) {
+    forEach(node.childNodes, function(child) {
+      output(child, win, buffer);
+    });
+    return;
+  }
+
+  switch (node.nodeType) {
+    case 1: // element
+      buffer.push(applyStyle(node, win));
+      break;
+
+    case 3: // text node
+      buffer.push(node.textContent);
+      break;
+  }
+}
+
+function hasChildren(node) {
+  return node.childNodes.length > 1 ||
+    (node.childNodes.length === 1 && node.childNodes[0].nodeType !== 3);
+}
+
+function applyStyle(node, win) {
   var text = node.textContent;
 
-  if (node.style.color) {
-    var color = nearestColor(node.style.color, supportedColors);
-    var sequence = escapeSequences[color.name];
+  var style = win.getComputedStyle(node);
+  if (style.color) {
+    var color = nearestColor(style.color, supportedColors);
+    var sequence = colorSequences[color.name];
     text = sequence[0] + text + sequence[1];
+  }
+
+  if (style.backgroundColor) {
+    var bgColor = nearestColor(style.backgroundColor, supportedColors);
+    var sequence = bgColorSequences[bgColor.name];
+    text = sequence[0] + text + sequence[1];
+  }
+
+  if (style.fontStyle === 'italic') {
+    text = styleSequences.italic[0] + text + styleSequences.italic[1];
+  }
+
+  if (style.fontWeight === 'bold') {
+    text = styleSequences.bold[0] + text + styleSequences.bold[1];
+  }
+
+  if (style.textDecoration === 'underline') {
+    text = styleSequences.underline[0] + text + styleSequences.underline[1];
   }
 
   return text;
@@ -55,4 +114,4 @@ function forEach(collection, fn) {
   Array.prototype.forEach.call(collection, fn);
 }
 
-module.exports = output;
+module.exports = constyle;
